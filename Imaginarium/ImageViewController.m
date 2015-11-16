24a4b12cac89ad938ac356ab70bd4ebc7873700a
Admin <Admin@wire.local>
2015-11-16 11:39:03 +0200

    Initial

--- Imaginarium/ImageViewController.m
+++ Imaginarium/ImageViewController.m
@@ -8,30 +8,72 @@
 
 #import "ImageViewController.h"
 
-@interface ImageViewController ()
-
+@interface ImageViewController () <UIScrollViewDelegate>
+@property (nonatomic, strong) UIImageView *imageView;
+@property (nonatomic, strong) UIImage *image;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
 @end
 
 @implementation ImageViewController
 
+- (void)setScrollView:(UIScrollView *)scrollView {
+    _scrollView = scrollView;
+    _scrollView.minimumZoomScale = 0.2;
+    _scrollView.maximumZoomScale = 2.0;
+    _scrollView.delegate = self;
+    self.scrollView.contentSize = self.image ? self.image.size : CGSizeZero;
+}
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+    return self.imageView;
+}
+
+- (void)setImageURL:(NSURL *)imageURL {
+    _imageURL = imageURL;
+    [self startDownloadImage];
+}
+
+- (void)startDownloadImage {
+    self.image = nil;
+    if (self.imageURL) {
+        [self.spinner startAnimating];
+        NSURLRequest *request = [NSURLRequest requestWithURL:self.imageURL];
+        NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration ephemeralSessionConfiguration];
+        NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
+        NSURLSessionDownloadTask *task = [session downloadTaskWithRequest:request completionHandler:^(NSURL *tempFileLocation, NSURLResponse *response, NSError * error) {
+            if (!error) {
+                if ([response.URL isEqual:self.imageURL]) {
+                    UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:tempFileLocation]];
+                    dispatch_async(dispatch_get_main_queue(), ^{ self.image = image; });
+                }
+            }
+        }];
+        [task resume];
+    }
+}
+
+- (void)setImage:(UIImage *)image {
+    self.imageView.image = image;
+    [self.imageView sizeToFit];
+    self.scrollView.contentSize = self.image ? self.image.size : CGSizeZero;
+    [self.spinner stopAnimating];
+}
+
+- (UIImage *)image {
+    return self.imageView.image;
+}
+
+- (UIImageView *)imageView {
+    if (!_imageView) {
+        _imageView = [[UIImageView alloc] init];
+    }
+    return _imageView;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self.scrollView addSubview:self.imageView];
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
--- Imaginarium/ViewController.m
+++ Imaginarium/ViewController.m
@@ -7,21 +7,137 @@
 //
 
 #import "ViewController.h"
+#import "ImageViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) NSMutableArray *tableData;
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (void)setTableView:(UITableView *)tableView {
+    _tableView = tableView;
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - Life cycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.tableData = [@[
+    @"abstract",
+//    @"animals",
+//    @"business",
+//    @"cats",
+//    @"city",
+//    @"food",
+//    @"night",
+//    @"life",
+//    @"fashion",
+//    @"people",
+//    @"nature",
+//    @"sports",
+//    @"technics",
+    @"transport"] mutableCopy];
+}
+
+#pragma mark - UITableView DataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.tableData count] + 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *const reuseIdentifierItem = @"cell for item";
+    static NSString *const reuseIdentifierAddItem = @"cell for add item";
+    
+    UITableViewCell *cell;
+    if (indexPath.row == self.tableData.count) {
+        cell = [tableView dequeueReusableCellWithIdentifier:reuseIdentifierAddItem];
+        cell.textLabel.text = @"Add Item...";
+    } else {
+        cell = [tableView dequeueReusableCellWithIdentifier:reuseIdentifierItem];
+        NSString *item = self.tableData[indexPath.row];
+        cell.textLabel.text = item;
+    }
+    
+    return cell;
+}
+
+#pragma mark - UITableView Delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.row == [self.tableData count]) {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Add Item" message:nil preferredStyle:UIAlertControllerStyleAlert];
+        
+        [alert addAction:[UIAlertAction actionWithTitle:@"Cancel" style:(UIAlertActionStyleCancel) handler:nil]];
+        
+        [alert addAction:[UIAlertAction actionWithTitle:@"Add" style:(UIAlertActionStyleDefault) handler:^(UIAlertAction * _Nonnull action) {
+            UITextField *textField = alert.textFields.firstObject;
+            NSString *text = textField.text;
+            if (text.length) {
+                [self.tableView beginUpdates];
+                
+                
+                NSIndexPath *insertedIndexPath = [NSIndexPath
+                                                  indexPathForRow:self.tableData.count
+                                                  inSection:0];
+                [self.tableData addObject:text];
+                [self.tableView insertRowsAtIndexPaths:@[insertedIndexPath] withRowAnimation:(UITableViewRowAnimationRight)];
+                
+                [self.tableView endUpdates];
+            }
+        }]];
+        
+        [alert addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+            textField.placeholder = @"Input catagory";
+        }];
+        
+        [self presentViewController:alert animated:YES completion:nil];
+    }
+}
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    return indexPath.row < self.tableData.count;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+}
+
+- (NSArray<UITableViewRowAction *> *)tableView:(UITableView *)tableView editActionsForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    
+    UITableViewRowAction *actionMore = [UITableViewRowAction rowActionWithStyle:(UITableViewRowActionStyleDefault) title:@"More" handler:^(UITableViewRowAction * _Nonnull action, NSIndexPath * _Nonnull indexPath) {
+        NSLog(@"Need to implement More action");
+    }];
+    actionMore.backgroundColor = [UIColor blueColor];
+    
+    UITableViewRowAction *actionDelete = [UITableViewRowAction rowActionWithStyle:(UITableViewRowActionStyleDestructive) title:@"Delete" handler:^(UITableViewRowAction * _Nonnull action, NSIndexPath * _Nonnull indexPath) {
+        [self.tableView beginUpdates];
+        
+        [self.tableData removeObjectAtIndex:indexPath.row];
+        [self.tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationLeft];
+        
+        [self.tableView endUpdates];
+    }];
+    
+    return @[actionMore, actionDelete];
+}
+
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if ([segue.destinationViewController isKindOfClass:[ImageViewController class]] &&
+        [sender isKindOfClass:[UITableViewCell class]]) {
+        NSIndexPath *indexPath = [self.tableView indexPathForCell:sender];
+        NSString *item = self.tableData[indexPath.row];
+        ImageViewController *ivc = segue.destinationViewController;
+        ivc.imageURL = [NSURL URLWithString:[NSString stringWithFormat:@"http://lorempixel.com/900/900/%@/", item]];
+    }
 }
 
 @end
